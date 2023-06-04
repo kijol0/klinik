454da4910ae7ab250fa0332490012081a642e16e
kijol0 <mrizalfauzi3@gmail.com>
2023-06-05 04:34:58 +0700

    ganti style dan obat selesai

--- resources/views/dashboard/obat/create.blade.php
+++ resources/views/dashboard/obat/create.blade.php
@@ -1,7 +1,7 @@
 @extends('dashboard.layouts.main')
 @section('container')
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Wilayah Specialis </h1>
+    <h1 class="h2">Tambah Data Obat </h1>
     
   </div>
   @if(session()->has('success'))
@@ -11,48 +11,45 @@
   </div>
   @endif
 
-  <div class="col-lg-8">
-      <form method="post" action="/dashboard/jadwal">
+  {{-- <div class="col-lg-8">
+      <form method="post" action="/dashboard/obat">
         @csrf
-        <div class="mb-3">
-          <label for="tanggal" class="form-label">Tanggal</label>
-          <input type="date" class="form-control" 
-          {{-- @error('tanggal') is-invalid @enderror  --}}
-          id="tanggal" name="tanggal">
-          {{-- @error('tanggal')
+        <div class="form-floating">
+          <input type="text" name="name"class="form-control rounded-top @error('name')
+          is-invalid @enderror" id="name" placeholder="Name" required value="{{ old('name') }}">
+          <label for="name">Name</label>
+          @error('name')
           <div class="invalid-feedback">
-            {{ $message }}
-          </div>
-          @enderror --}}
+              {{ $message }}
+            </div>
+            @enderror
         </div>
-        <div class="mb-3">
-            <label for="lapangan" class="form-label">Lapangan</label>
-            <select class="form-select" name="lapangan_id">
-                @foreach($lapangans as $lapangan)
-                @if(old('lapangan_id')==$lapangan->id)
-                <option value="{{ $lapangan->id }}" selected> {{$lapangan->name}}</option>
-                @else 
-                <option value="{{ $lapangan->id }}"> {{$lapangan->name}}</option>
-                @endif
-                @endforeach
-              </select>
-            
-          </div>
-
-          <div class="mb-3">
-            <label for="jam" class="form-label">Jam</label>
-            <select class="form-select" name="jam_id">
-                @foreach($jams as $jam)
-                @if(old('jam_id')==$jam->id)
-                <option value="{{ $jam->id }}" selected>{{$jam->jam}}</option>
-                @else 
-                <option value="{{ $jam->id }}">{{$jam->jam}}</option>
-                @endif
-                
-                @endforeach
-              </select>
-          </div>
+        <div class="form-floating">
+          <input type="text" name="harga"class="form-control rounded-top @error('harga')
+          is-invalid @enderror" id="harga" placeholder="Harga" required value="{{ old('harga') }}">
+          <label for="name">Harga</label>
+          @error('harga')
+          <div class="invalid-feedback">
+              {{ $message }}
+            </div>
+            @enderror
+        </div>     --}}
         
+          <div class="col-lg-8">
+              <form method="post" action="/dashboard/obat">
+                @csrf
+                <div class="mb-3">
+                  <label for="name" class="form-label">Nama obat</label>
+                  <input type="text" class="form-control" id="name" name="name">
+                  
+                </div>
+                <div class="mb-3">
+                    <label for="harga" class="form-label">harga</label>
+                    <input type="text" class="form-control" id="harga" name="harga">
+                    
+                  </div>
+        
+
         <button type="submit" class="btn btn-primary">Tambah</button>
       </form>
   </div>
